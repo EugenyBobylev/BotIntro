8bf30ad88edcdf3a7ac723455d46275435b1d1ad
Eugeny Bobylev <bea@hotbox.ru>
2020-05-27 05:14:36 +1000

    Заработали кнопки в боте

--- app/bot/BotIntro.py
+++ app/bot/BotIntro.py
@@ -1,14 +1,41 @@
-import config
+import app.bot.config
 import telebot
 
 
-
-bot = telebot.TeleBot(config.token)
+bot = telebot.TeleBot(app.bot.config.token)
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+
+
+@bot.message_handler(commands=['gettasks'])
+def get_tasks(message):
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    keyboard.row(
+        telebot.types.InlineKeyboardButton('Все не завершенные', callback_data='get_all_tasks'),
+    )
+    keyboard.row(
+        telebot.types.InlineKeyboardButton('Все на сегодня', callback_data='get_today_tasks'),
+        telebot.types.InlineKeyboardButton('Все на завтра', callback_data='get_tomorrow_tasks')
+    )
+    bot.send_message(message.chat.id, 'Выберите', reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(query):
+    data = query.data
+    func = get_tasks_dict[data]
+    func(query.message)
+
+
+@bot.message_handler(commands=['url'])
+def btn_test(message):
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    url_button = telebot.types.InlineKeyboardButton(text="Перейти на Яндекс", url="https://ya.ru")
+    keyboard.add(url_button)
+    bot.send_message(message.chat.id, 'Нажми кнопку получишь результат', reply_markup=keyboard)
 
 
 @bot.message_handler(content_types=['text'])
@@ -29,5 +56,26 @@ def sticker_id(message):
     print(message)
 
 
+@bot.message_handler(func=lambda message: True)
+def get_all_tasks(message):
+    bot.reply_to(message, 'выполняется get_all_tasks')
+
+
+@bot.message_handler(func=lambda message: True)
+def get_today_tasks(message):
+    bot.reply_to(message, 'выполняется get_today_tasks')
+
+
+@bot.message_handler(func=lambda message: True)
+def get_tomorrow_tasks(message):
+    bot.reply_to(message, 'выполняется get_tomorrow_tasks')
+
+
 if __name__ == "__main__":
-    bot.infinity_polling()
+    get_tasks_dict = {
+        'get_all_tasks': get_all_tasks,
+        'get_today_tasks': get_today_tasks,
+        'get_tomorrow_tasks': get_tomorrow_tasks
+    }
+
+    bot.polling()
