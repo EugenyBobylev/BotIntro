8b6725204aae681b132154e48260ed0d426bf928
Bobylev <ebobylev@gmailcom>
2020-05-28 10:38:02 +1000

    добавлено главное меню бота

--- app/bot/BotIntro.py
+++ app/bot/BotIntro.py
@@ -1,5 +1,6 @@
 import app.bot.config
 import telebot
+from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
 from app.bot.TCalendar import create_calendar, calendar_callback
 
 bot = telebot.TeleBot(app.bot.config.token)
@@ -7,26 +8,17 @@ bot = telebot.TeleBot(app.bot.config.token)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+    bot.send_message(message.chat.id, 'Начинаем работу /start')
+    keyboard = InlineKeyboardMarkup()
+    keyboard.add(InlineKeyboardButton('Добавить задачу', callback_data='add_new_task'))
+    keyboard.add(InlineKeyboardButton('Мои задачи', callback_data='show_tasks'))
+    bot.send_message(message.chat.id, 'Выберите дальнейшее действие', reply_markup=keyboard)
 
 
 @bot.message_handler(commands='calendar')
 def get_calendar(message):
     keyboard = create_calendar()
     bot.send_message(message.chat.id, 'Выберите дату', reply_markup=keyboard)
-
-
-@bot.message_handler(commands=['gettasks'])
-def get_tasks(message):
-    keyboard = telebot.types.InlineKeyboardMarkup()
-    keyboard.row(
-        telebot.types.InlineKeyboardButton('Все не завершенные', callback_data='get_all_tasks'),
-    )
-    keyboard.row(
-        telebot.types.InlineKeyboardButton('Все на сегодня', callback_data='get_today_tasks'),
-        telebot.types.InlineKeyboardButton('Все на завтра', callback_data='get_tomorrow_tasks')
-    )
-    bot.send_message(message.chat.id, 'Выберите', reply_markup=keyboard)
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -60,6 +52,29 @@ def sticker_id(message):
     print(message)
 
 
+# --------------------------------------------------------
+@bot.message_handler(content_types=['sticker'])
+def get_home(message):
+    bot.reply_to(message, 'выполняется get_home')
+
+
+@bot.message_handler(content_types=['sticker'])
+def add_new_task(message):
+    bot.reply_to(message, 'выполняется add_new_task')
+
+
+@bot.message_handler(func=lambda message: True)
+def show_tasks(message):
+    keyboard = InlineKeyboardMarkup()
+
+    keyboard.add(InlineKeyboardButton('Все не завершенные', callback_data='get_all_tasks')),
+    keyboard.add(InlineKeyboardButton('Все на сегодня', callback_data='get_today_tasks')),
+    keyboard.add(InlineKeyboardButton('Все на завтра', callback_data='get_tomorrow_tasks'))
+    keyboard.add(InlineKeyboardButton('Вернуться', callback_data='get_home'))
+
+    bot.send_message(message.chat.id, 'Выберите', reply_markup=keyboard)
+
+
 @bot.message_handler(func=lambda message: True)
 def get_all_tasks(message):
     bot.reply_to(message, 'выполняется get_all_tasks')
@@ -77,6 +92,9 @@ def get_tomorrow_tasks(message):
 
 if __name__ == "__main__":
     get_tasks_dict = {
+        'get_home': get_home,
+        'add_new_task': add_new_task,
+        'show_tasks': show_tasks,
         'get_all_tasks': get_all_tasks,
         'get_today_tasks': get_today_tasks,
         'get_tomorrow_tasks': get_tomorrow_tasks
