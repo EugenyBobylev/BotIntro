64c79565a6886d9c65436c722f535a5962220504
Bobylev <ebobylev@gmailcom>
2020-05-29 15:38:34 +1000

    удаляются сообщения

--- app/bot/tbot.py
+++ app/bot/tbot.py
@@ -1,16 +1,19 @@
 import telebot
+from app.bot.bot_stack import BotStack
 
 bot = telebot.TeleBot('1019358164:AAFGDWu1zn-nJyDKlKEFzFcuWBgYP-f30-Y')
+stack = BotStack()
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Начинаем работу /start')
+    msg = bot.send_message(message.chat.id, 'Начинаем работу /start')
+    push(msg)
 
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    bot.delete_message(message.chat.id, message.message_id-1)
+    clear_messages(message.chat.id)
     message.text = message.text.lower()
     if message.text == 'text':
         bot.send_message(message.chat.id, 'Привет')
@@ -22,6 +25,20 @@ def send_text(message):
         bot.send_sticker(message.chat.id, 'CAADAgADZgkAAnlc4gmfCor5YbYYRAI')
     else:
         bot.delete_message(message.chat.id, message.message_id)
+
+
+def clear_messages(chat_id):
+    while stack.count(chat_id) > 0:
+        msg_id = pop(chat_id)
+        bot.delete_message(chat_id, msg_id)
+
+def push(message):
+    stack.push(message.chat.id, message.message_id)
+
+
+def pop(chat_id):
+    message_id = stack.pop(chat_id)
+    return message_id
 
 
 if __name__ == '__main__':
